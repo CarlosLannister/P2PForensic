15152b498b4e4c803ea7ca9363b7ffa9219dd151
CarlosLannister <carloslannister@gmail.com>
2016-09-08 23:13:44 +0200

    Added extraction of download.txt

--- emuleForensic.py
+++ emuleForensic.py
@@ -34,24 +34,48 @@
 import jarray
 import inspect
 import os
+
 from java.lang import System
+from java.sql  import DriverManager, SQLException
 from java.util.logging import Level
-from org.sleuthkit.datamodel import SleuthkitCase
-from org.sleuthkit.datamodel import AbstractFile
-from org.sleuthkit.datamodel import ReadContentInputStream
-from org.sleuthkit.datamodel import BlackboardArtifact
-from org.sleuthkit.datamodel import BlackboardAttribute
-from org.sleuthkit.autopsy.ingest import IngestModule
-from org.sleuthkit.autopsy.ingest.IngestModule import IngestModuleException
-from org.sleuthkit.autopsy.ingest import DataSourceIngestModule
-from org.sleuthkit.autopsy.ingest import FileIngestModule
-from org.sleuthkit.autopsy.ingest import IngestModuleFactoryAdapter
-from org.sleuthkit.autopsy.ingest import IngestMessage
-from org.sleuthkit.autopsy.ingest import IngestServices
-from org.sleuthkit.autopsy.coreutils import Logger
+from java.io import File
+from java.awt import BorderLayout
+from javax.swing import BorderFactory
+from javax.swing import JTextArea
+from javax.swing import JScrollPane
+from javax.swing import JButton
+from javax.swing import JToolBar
+from javax.swing import JPanel
+from javax.swing import JFrame
+from javax.swing import JCheckBox
+from javax.swing import JTextField
+from javax.swing import JLabel
+from javax.swing import JFileChooser
+from javax.swing.filechooser import FileNameExtensionFilter
 from org.sleuthkit.autopsy.casemodule import Case
 from org.sleuthkit.autopsy.casemodule.services import Services
+from org.sleuthkit.autopsy.ingest import DataSourceIngestModule
+from org.sleuthkit.autopsy.ingest import FileIngestModule
+from org.sleuthkit.autopsy.ingest import IngestMessage
+from org.sleuthkit.autopsy.ingest import IngestModule
+from org.sleuthkit.autopsy.ingest.IngestModule import IngestModuleException
+from org.sleuthkit.autopsy.ingest import IngestModuleFactoryAdapter
+from org.sleuthkit.autopsy.ingest import IngestModuleIngestJobSettings
+from org.sleuthkit.autopsy.ingest import IngestModuleIngestJobSettingsPanel
+from org.sleuthkit.autopsy.ingest import IngestServices
+from org.sleuthkit.autopsy.ingest import IngestModuleGlobalSettingsPanel
+from org.sleuthkit.datamodel import SleuthkitCase
+from org.sleuthkit.datamodel import AbstractFile
+from org.sleuthkit.datamodel import BlackboardArtifact
+from org.sleuthkit.datamodel import BlackboardAttribute
+from org.sleuthkit.datamodel import ReadContentInputStream
+from org.sleuthkit.autopsy.coreutils import Logger
+from java.lang import IllegalArgumentException
+from org.sleuthkit.autopsy.ingest import DataSourceIngestModule
+from org.sleuthkit.autopsy.ingest import ModuleDataEvent
 from org.sleuthkit.autopsy.casemodule.services import FileManager
+from org.sleuthkit.autopsy.datamodel import ContentUtils
+
 # This will work in 4.0.1 and beyond
 # from org.sleuthkit.autopsy.casemodule.services import Blackboard
 
@@ -113,15 +137,7 @@ class EmuleDataSourceIngestModule(DataSourceIngestModule):
 
         # we don't know how much work there is yet
         progressBar.switchToIndeterminate()
-
-        # This will work in 4.0.1 and beyond
-        # Use blackboard class to index blackboard artifacts for keyword search
-        # blackboard = Case.getCurrentCase().getServices().getBlackboard()
-
-        # For our example, we will use FileManager to get all
-        # files with the word "test"
-        # in the name and then count and read them
-        # FileManager API: http://sleuthkit.org/autopsy/docs/api-docs/3.1/classorg_1_1sleuthkit_1_1autopsy_1_1casemodule_1_1services_1_1_file_manager.html
+   
         fileManager = Case.getCurrentCase().getServices().getFileManager()
 
         emuleConfigFiles = fileManager.findFiles(dataSource, "%", "/AppData/Local/eMule/config")
@@ -143,13 +159,37 @@ class EmuleDataSourceIngestModule(DataSourceIngestModule):
                 return IngestModule.ProcessResult.OK
 
             self.log(Level.INFO, "Processing file: " + str(file.getName()))
-            if "downloads.bak" in file.getName():
+            if "downloads.txt" in file.getName():
                 configFilesPath = os.path.join(Case.getCurrentCase().getTempDirectory(), str(file.getName()))
                 ContentUtils.writeToFile(file, File(configFilesPath))
 
                 art = file.newArtifact(BlackboardArtifact.ARTIFACT_TYPE.TSK_INTERESTING_FILE_HIT)
-                att = BlackboardAttribute(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_SET_NAME, EmuleIngestModuleFactory.moduleName, str(file.getName()))
-                art.addAttribute(att)
+
+
+                art.addAttribute(BlackboardAttribute(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_SET_NAME, EmuleIngestModuleFactory.moduleName, "Emule4 Info"))
+                #att = BlackboardAttribute(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_SET_NAME, EmuleIngestModuleFactory.moduleName, str(file.getName()))
+                #art.addAttribute(att)
+
+                self.log(Level.INFO, "Path")
+
+                self.log(Level.INFO, configFilesPath)
+  
+                f = open(configFilesPath, 'r')
+                reportPath = os.path.join(Case.getCurrentCase().getTempDirectory(), "report")
+                report = open(reportPath, 'w')
+
+
+                for i, line in enumerate(f):
+                    if i > 4:
+                        self.log(Level.INFO, "Linea3 ")
+                        line = line.strip()
+
+                        report.write(line + "\n")
+                    
+
+                
+                f.close()
+                report.close()
 
         self.log(Level.INFO, "Fin ")
         #Post a message to the ingest messages in box.
